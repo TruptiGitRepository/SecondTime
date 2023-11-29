00e6fb83d700f35af973d571f58f8d46f1f864fc
Trupti <d_trupti22@yahoo.com>
2023-11-29 12:00:54 -0500

    commiting to notmain branch

---
+++ Palindrome_ForNotMainbranch.java
@@ -0,0 +1,29 @@
+//Adding this for notmain branch to test branching in git
+public class Palindrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int Pali= 5335;
+		 String s="2level";
+		 Solution1 so1=new Solution1();
+       boolean b = so1.isPalindrome(Pali);
+       boolean c= so1.isStrPalindrome(s);
+       System.out.println(b);
+       System.out.println(c);
+	}
+
+}
+
+class Solution1 {
+	boolean bool;
+    public boolean isPalindrome(int  x) {
+    	String intStr = String.valueOf(x); 
+        bool= intStr.equals(new StringBuilder(intStr).reverse().toString());
+        return bool;
+    }
+    public boolean isStrPalindrome(String  x) {
+    	
+        bool= x.equals(new StringBuilder(x).reverse().toString());
+        return bool;
+    }
+}
